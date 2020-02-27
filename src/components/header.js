e13f80f5d125536894c3684f09c6dc1e5ff9a8b6
Terry Reynolds <terrancejreynolds@gmail.com>
2020-02-26 22:40:57 -0500

    set up done. Wrote the nav page with strobe.

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+  <header className="grid_item" id="name">
+    TERRY REYNOLDS <em id="sub_name">Developer, Teacher</em>
+    <hr id="line1" />
+    <hr id="line2" />
+  </header>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,115 @@
 import React from "react"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      grid_item_1: false,
+      grid_item_2: false,
+      grid_item_3: false,
+      strobeInterval: "",
+    }
+  }
+
+  componentDidMount() {
+    const strobe = setInterval(this.strobeNav, 1000)
+    this.setState({ strobeInterval: strobe })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.strobeInterval)
+  }
+
+  strobeNav = () => {
+    console.log("instrobeNav")
+
+    setTimeout(() => {
+      this.setState({
+        grid_item_1: true,
+      })
+    }, 100)
+
+    setTimeout(() => {
+      this.setState({
+        grid_item_1: false,
+        grid_item_2: true,
+      })
+    }, 200)
+
+    setTimeout(() => {
+      this.setState({
+        grid_item_1: false,
+        grid_item_2: false,
+        grid_item_3: true,
+      })
+    }, 300)
+
+    setTimeout(() => {
+      this.setState({
+        grid_item_1: false,
+        grid_item_2: false,
+        grid_item_3: false,
+      })
+    }, 400)
+  }
+
+  handleClick = () => {
+    console.log("inhandleClick")
+  }
+  render() {
+    return (
+      <div className="grid_container" id="navbar">
+        <Header />
+
+        <div
+          className={
+            this.state.grid_item_1 ? "grid_item_1_On " : "grid_item_1_Off"
+          }
+          id="about_color"
+        ></div>
+        <nav
+          className="grid_item"
+          id="about"
+          href="about"
+          onClick={this.handleClick}
+        >
+          ABOUT
+        </nav>
+
+        <div
+          className={
+            this.state.grid_item_2 ? "grid_item_2_On " : "grid_item_2_Off"
+          }
+          id="projects_color"
+        ></div>
+        <nav
+          className="grid_item"
+          id="projects"
+          href="projects"
+          onClick={this.handleClick}
+        >
+          PROJECTS
+        </nav>
+
+        <div
+          className={
+            this.state.grid_item_3 ? "grid_item_3_On " : "grid_item_3_Off"
+          }
+          id="contact_color"
+        ></div>
+        <nav
+          className="grid_item"
+          id="contact"
+          href="contact"
+          onClick={this.handleClick}
+        >
+          CONTACT
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Nav
