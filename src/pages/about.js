1d7de3373ede74e5aa2de91421ac5aa0a70c3e19
Terry Reynolds <terrancejreynolds@gmail.com>
2020-07-19 22:57:46 -0400

    made About stateless and added image plugin to bio pic

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,64 +1,77 @@
 import React from "react"
 import Header from "../components/header"
-import TerryImg from "../assets/terry-square.jpg"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import Burger from "../components/burger"
 import { navigate } from "gatsby"
-class About extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      dog: true,
-    }
-  }
-  handleClick = e => {
+
+const About = ({ data }) => {
+  const handleClick = e => {
     e.preventDefault()
     console.log("inabouthandleClick")
     navigate("/")
   }
-  render() {
-    return (
-      <div className="about_container" id="about_page">
-        <div className="about_item" id="about_name">
-          <Header />
-        </div>
-        <div
-          role="link"
-          tabIndex={0}
-          id="hamburger_menu"
-          onClick={this.handleClick}
-          onKeyDown={this.handleClick}
-        >
-          <Burger />
-        </div>
 
-        <div id="about_color_long"></div>
-        <div id="about_picture">
-          <img
-            src={TerryImg}
-            id="terry_photo"
-            alt="terry illuminated with blue and red light"
-          />
-        </div>
-        <article id="about_bio">
-          <div id="about_text">
-            Experienced Developer and Teacher with a demonstrated history of
-            teaching Primary, Junior and Intermediate levels in the Ontario
-            public school system. Skilled in
-            <span className="spanInPara">FullStack Web Development</span>,
-            specializing in <span>Javascript</span>, <span>HTML5</span>,
-            <span>CSS</span> and related libraries/frameworks (<span>Node</span>
-            , <span>React</span>, <span>Gatsby</span>,<span>Bootstrap</span>,{" "}
-            <span>CSS Grid</span>, <span>Flex</span>,<span>Typography.js</span>{" "}
-            and <span>JQuery</span> ) as well as the
-            <span>MERN Stack</span> including <span>GraphQL</span>. Strong
-            education professional with a Master of Science in Teaching. Skilled
-            copy-editor with a background in English Literature. Eight years
-            experience in the electronic manufacturing industry (Nortel/Brock
-            Telecom).
-          </div>
-        </article>
+  return (
+    <div className="about_container" id="about_page">
+      <div className="about_item" id="about_name">
+        <Header />
       </div>
-    )
-  }
+      <div
+        role="link"
+        tabIndex={0}
+        id="hamburger_menu"
+        onClick={handleClick}
+        onKeyDown={handleClick}
+      >
+        <Burger />
+      </div>
+
+      <div id="about_color_long"></div>
+      <div id="about_picture">
+        <Img
+          className="terry_photo"
+          fluid={data.terry.childImageSharp.fluid}
+          alt="terry illuminated with blue and red light"
+        />
+      </div>
+      <article id="about_bio">
+        <div id="about_text">
+          Experienced Developer and Teacher with a demonstrated history of
+          teaching Primary, Junior and Intermediate levels in the Ontario public
+          school system. Skilled in
+          <span className="spanInPara">FullStack Web Development</span>,
+          specializing in <span>Javascript</span>, <span>HTML5</span>,
+          <span>CSS</span> and related libraries/frameworks (<span>Node</span>,{" "}
+          <span>React</span>, <span>Gatsby</span>,<span>Bootstrap</span>,{" "}
+          <span>CSS Grid</span>, <span>Flex</span>,<span>Typography.js</span>{" "}
+          and <span>JQuery</span> ) as well as the
+          <span>MERN Stack</span> including <span>GraphQL</span>. Strong
+          education professional with a Master of Science in Teaching. Skilled
+          copy-editor with a background in English Literature. Eight years
+          experience in the electronic manufacturing industry (Nortel/Brock
+          Telecom).
+        </div>
+      </article>
+    </div>
+  )
 }
+
+export const squareImage = graphql`
+  fragment squareImage on File {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`
+
+export const query = graphql`
+  query {
+    terry: file(relativePath: { eq: "terry.jpg" }) {
+      ...squareImage
+    }
+  }
+`
 export default About
